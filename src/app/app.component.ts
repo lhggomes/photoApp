6d72c88b048b30f9f0238c6ce1d2f7666c620dfe
Gomes, Lucas Henrique Silva <lucas.h.gomes@accenture.com>
2021-12-26 23:24:08 -0300

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PhotoApp';
+  photos = [
+    {
+      url: "https://disneyplusbrasil.com.br/wp-content/uploads/2021/03/Homem-Aranha-Meme.jpg",
+      description:  "Spider"
+    },
+    {
+      url: "https://sm.ign.com/ign_br/screenshot/default/harry-potter-hbo-max_q8yn.jpg",
+      description:  "Harry Potter"
+    }
+  ];
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
+import { PhotosModule } from './photos/photos.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
+    PhotosModule,
     AppRoutingModule
   ],
   providers: [],
