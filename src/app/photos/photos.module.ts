3b2ee2d93916763648f70f524c49cfab2cc21f98
Gomes, Lucas Henrique Silva <lucas.h.gomes@accenture.com>
2021-12-28 22:47:19 -0300

    New Module

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -4,10 +4,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { PhotoListComponent } from './photo-list/photo-list.component';
 import { PhotoComponent } from './photo/photo.component';
 import { PhotoFormComponent } from './photo-form/photo-form.component';
+import { PhotosComponent } from './photo-list/photos/photos.component';
 
 
 @NgModule({
-    declarations: [PhotoComponent, PhotoListComponent, PhotoFormComponent ],
+    declarations: [PhotoComponent, PhotoListComponent, PhotoFormComponent, PhotosComponent ],
     exports: [],
     imports: [ HttpClientModule, CommonModule]
 })
