2ea579403c2dc878568529a467535c14fa154d25
Gomes, Lucas Henrique Silva <lucas.h.gomes@accenture.com>
2022-01-02 00:53:55 -0300

    Starting with authenticator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { PhotosModule } from './photos/photos.module';
 import { AppRoutingModule } from './app.routing.module';
 import { ErrorsModule } from './errors/errors.module';
+import {HomeModule} from './home/home.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { ErrorsModule } from './errors/errors.module';
   imports: [
     BrowserModule,
     PhotosModule,
+    HomeModule,
     AppRoutingModule,
     ErrorsModule
   ],
