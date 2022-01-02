22d28721570f53ab2bc7b72228990172491ae8fb
Gomes, Lucas Henrique Silva <lucas.h.gomes@accenture.com>
2022-01-02 01:04:45 -0300

    Starting with authenticator

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,10 +2,11 @@ import {NgModule} from '@angular/core';
 import {SignInComponent} from './signin/signin.component';
 import {ReactiveFormsModule} from '@angular/forms';
 import {CommonModule} from '@angular/common';
+import {VmessageModule} from '../shared/components/vmessage/vmessage.module';
 
 
 @NgModule({
   declarations: [SignInComponent],
-  imports: [ReactiveFormsModule, CommonModule]
+  imports: [ReactiveFormsModule, CommonModule, VmessageModule]
 })
 export class HomeModule {}
