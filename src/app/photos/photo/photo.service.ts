a6c9fe761d0b974c2f57637186c8aa781104810c
Gomes, Lucas Henrique Silva <lucas.h.gomes@accenture.com>
2022-01-01 22:49:22 -0300

    Loading button

--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -1,7 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-import { Photo } from './photo';
+import { Photo } from "./photo";
 
 const API = 'http://localhost:3000';
 
@@ -12,6 +12,14 @@ export class PhotoService {
 
     listFromUser(userName: string) {
         return this.http
-            .get<Photo[]>(API + '/' + userName + '/photos');
+            .get<Photo[]>(API + '/' + userName + '/photos');       
     }
+
+    listFromUserPaginated(userName: string, page: number) {
+        const params = new HttpParams()
+            .append('page', page.toString());
+
+        return this.http
+            .get<Photo[]>(API + '/' + userName + '/photos', { params });       
+    }    
 }
