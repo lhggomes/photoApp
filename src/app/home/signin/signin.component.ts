374b938c6227700f041978d0684523caf6565586
Gomes, Lucas Henrique Silva <lucas.h.gomes@accenture.com>
2022-01-03 21:21:25 -0300

    Checking Platform

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,15 +1,21 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {AuthService} from '../../core/auth.service';
+import {AuthService} from '../../core/auth/auth.service';
 import {Router} from '@angular/router';
+import {PlataformDetectorService} from '../../core/platform/plataform-detector.service';
 
 @Component({
   templateUrl: './signin.component.html'
 })
 export class SignInComponent implements OnInit {
   loginForm: FormGroup;
+  @ViewChild('userNameInput') userNameInput: ElementRef<HTMLInputElement>;
 
-  constructor(private formBuilder: FormBuilder, private authService: AuthService, private router: Router) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router,
+    private platformDetectorService: PlataformDetectorService) {
   }
 
   ngOnInit(): void {
@@ -24,10 +30,12 @@ export class SignInComponent implements OnInit {
     const password = this.loginForm.get('password').value;
     this.authService.authenticate(userName, password)
       .subscribe(() => this.router.navigate(['user', userName]),
-          error => {
-        console.log(error);
-        this.loginForm.reset();
-        alert('Invalid Username or password');
-      });
+        error => {
+          console.log(error);
+          this.loginForm.reset();
+          // tslint:disable-next-line:no-unused-expression
+          this.platformDetectorService.isPlatformBrowser() && this.userNameInput.nativeElement.focus();
+          alert('Invalid Username or password');
+        });
   }
 }
